2463e5080c447b4104f36cf87fffbbb38bab1268
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-11-13 08:00:54 +0700

    shadcn

--- W_demo/be/src/main/java/hau/edu/chat_room/controllers/MainController.java
+++ W_demo/be/src/main/java/hau/edu/chat_room/controllers/MainController.java
@@ -1,3 +1,12 @@
-public class MainController {
+package hau.edu.chat_room.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/api/main")
+    public String mainEndpoint(){
+        return "This is the main endpoint of the Chat Room application.";
+    }
 }
