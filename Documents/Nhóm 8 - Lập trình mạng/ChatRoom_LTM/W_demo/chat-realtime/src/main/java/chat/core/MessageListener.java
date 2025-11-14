ee4c1ec124538a91f97f5de02bca19b2cb64bddb
ngar <tnga2756@gmail.com>
2025-11-14 23:24:27 +0700

    chore: Remove Documents directory

--- Documents/Nhóm 8 - Lập trình mạng/ChatRoom_LTM/W_demo/chat-realtime/src/main/java/chat/core/MessageListener.java
+++
@@ -1,9 +0,0 @@
-package chat.core;
-
-public interface MessageListener {
-    void onMessage(Message message);
-    default void onEvent(Object event) {}
-    default void onError(String error) {}
-    default void onDisconnect() {}
-    default void onAck(String cid) {}
-}
--- Documents/Nhóm 8 - Lập trình mạng/ChatRoom_LTM/W_demo/chat-realtime/src/main/java/chat/core/spi/AuthGateway.java
+++
@@ -1,9 +0,0 @@
-package chat.core.spi;
-
-public interface AuthGateway {
-    boolean authenticate(String username, String password);
-    boolean isUserInRoom(String username, String roomId);
-    boolean canUserJoinRoom(String username, String roomId);
-
-    default boolean isAuthenticated(String username) { return true; }
-}
