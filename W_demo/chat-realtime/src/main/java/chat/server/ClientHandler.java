20634c3dddd22e9aa9af66f845de027c06780099
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-11-14 19:07:11 +0700

    'reco_nga'

--- W_demo/chat-realtime/src/main/java/chat/server/ClientHandler.java
+++ W_demo/chat-realtime/src/main/java/chat/server/ClientHandler.java
@@ -34,6 +34,7 @@ class ClientHandler implements Runnable {
                 while ((message = reader.readLine()) != null) {
                     if (message.equalsIgnoreCase("exit")) break;
                     
+                    // Gửi tin nhắn đến tất cả client
                     server.broadcastMessage(username + ": " + message);
                 }
             }
